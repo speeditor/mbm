aecd8731cd3600985c94ad1be067537f0bc4b805
speeditor <zkrjeb@gmail.com>
2017-09-22 12:51:50 +0100

    created main.js for importing

---
+++ main.js
@@ -0,0 +1,43 @@
+// Monobook mobile wrapper (WIP)
+mw.loader.using(['mediawiki.util', 'jquery']).then(function () {
+    // == dependencies ==
+
+    // === CSS dependencies ===
+    mw.hook('dev.colors').add(function(module) {
+        dev.colors.css(
+        '.skin-monobook.mbm-on {' +
+            'background-color: $body;' +
+        '}' +
+        '.skin-monobook.mbm-on #column-content #mbm-bg {' +
+            'background: $page ;' +
+            'opacity: ' + wgSassParams['page-opacity']/100 + ';' +
+        '}' +
+        '.skin-monobook.mbm-on #content #firstHeading,' +
+        '.skin-monobook.mbm-on #content #siteSub,' +
+        '.skin-monobook.mbm-on #column-one .portlet,' +
+        '.skin-monobook.mbm-on #footer {' +
+            'background: $header;' +
+        '}' +
+        '.skin-monobook.mbm-on #column-one #p-logo {' +
+            'border-top: 4px solid $header !important;' +
+        '}' +
+        '.skin-monobook.mbm-on #globalWrapper a {' +
+            'color: $link;' +
+        '}')
+    });
+
+    // === JS dependencies ===
+    importArticles({
+        type: 'script',
+        articles: [
+            'u:dev:PersistentParameters/code.js',
+            'u:dev:Colors/code.js'
+        ]
+    });
+    
+    // == initialisation ==
+    importScriptURI('https://raw.github.com/speeditor/mbm/master/core.js');
+    if (navigator.userAgent.indexOf('Mobi') > -1) {
+        $('.skin-monobook').addClass('mbm-on');
+    }
+});
