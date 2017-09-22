274b3a0c0f1192498220ff96d9df30ecb079fb06
speeditor <zkrjeb@gmail.com>
2017-09-22 13:13:33 +0100

    cleanup

--- core.js
+++ core.js
@@ -1,67 +1,73 @@
 // Monobook mobile wrapper (WIP)
-    // responsive design
-    window.mbmstate = window.mbmstate ? true : false;
-    var mbmbtn = mw.html.element('div', { id: 'p-mbm' }, 'mbm');
-        mbmvpt = mw.html.element('meta', {
-                     name: 'viewport',
-                     content: 'user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, minimal-ui'
-                 });
-    $('head').append(mbmvpt);
-    $('.skin-monobook').append(mbmbtn);
-    $('#p-mbm').click(function(toggle) {
-        toggle.preventDefault();
-        window.mbmstate = window.mbmstate ? false : true;
-        $('.skin-monobook').toggleClass('mbm-on');
-    });
-    if (navigator.userAgent.indexOf('Android') > -1) {
-        $('head').append(mw.html.element('meta', { name: 'theme-color', content: dev.colors.wikia.header }));
-    } else if (navigator.userAgent.indexOf('iPhone') > -1) {
-        $('head').append(mw.html.element('meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' }));
+// == global variables ==
+window.mbmstate = window.mbmstate ? true : false;
+
+// == user interface ==
+var mbmbtn = mw.html.element('div', { id: 'p-mbm' }, 'mbm');
+$('.skin-monobook').append(mbmbtn);
+if (navigator.userAgent.indexOf('Android') > -1) {
+    $('head').append(mw.html.element('meta', { name: 'theme-color', content: dev.colors.wikia.header }));
+} else if (navigator.userAgent.indexOf('iPhone') > -1) {
+    $('head').append(mw.html.element('meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' }));
+}
+
+// == responsive design ==
+var mbmvpt = mw.html.element('meta', {
+                 name: 'viewport',
+                 content: 'user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, minimal-ui'
+             });
+$('head').append(mbmvpt);
+$('#p-mbm').click(function(toggle) {
+    toggle.preventDefault();
+    window.mbmstate = window.mbmstate ? false : true;
+    $('.skin-monobook').toggleClass('mbm-on');
+});
+
+// == interface elements ==
+var navbtn = mw.html.element('div', { id: 'p-navicon' }),
+    schbtn = mw.html.element('div', { id: 'p-schicon' }),
+    wbgmbm = mw.html.element('div', { id: 'mbm-bg' }),
+    viscls = 'is-visible',
+    schcls = 'is-search';
+// === toolbar buttons ===
+$('#p-logo').append(navbtn);
+$('#p-logo').append(schbtn);
+$('#column-content').append(wbgmbm);
+// == toolbar initialization ===
+$('#column-content').addClass(viscls);
+$('#footer').addClass(viscls);
+
+// == interactivity ==
+// === wiki navigation menu ===
+$('#p-navicon').click(function(navmbm) {
+    navmbm.preventDefault();
+    $('#column-one').toggleClass(viscls);
+    $('#column-content').toggleClass(viscls);
+    $('#footer').toggleClass(viscls);
+    $('#p-search').removeClass(viscls);
+    $('#column-one').removeClass(schcls);
+    $('.portlet').removeClass(viscls);
+});
+// === search functionality ===
+$('#p-schicon').click(function(schmbm) {
+    schmbm.preventDefault();
+    $('#p-search').toggleClass(viscls);
+    $('#column-one').toggleClass(schcls);
+});
+$('#searchInput').attr('placeholder','Search the ' + wgSitename);
+// === wiki submenu navigation ===
+$('.portlet > h5').click(function(menumbm) {
+    menumbm.preventDefault();
+    $(this).parent().toggleClass(viscls);
+});
+
+// == content adaptability ==
+$('p > br:only-child').parent().remove();
+$(window).resize(function() {
+    if ($('.wikia-slideshow-images').length) {
+        $('.wikia-slideshow-images').each(function(mbmslideshow) {
+            $(this).css('width', $(this).parent().width());
+            $(this).css('height', $(this).parent().width()*0.75);
+        });
     }
-    // variables & UI
-    var navbtn = mw.html.element('div', { id: 'p-navicon' }),
-        schbtn = mw.html.element('div', { id: 'p-schicon' }),
-        wbgmbm = mw.html.element('div', { id: 'mbm-bg' }),
-        viscls = 'is-visible',
-        schcls = 'is-search';
-    // toolbar buttons
-    $('#p-logo').append(navbtn);
-    $('#p-logo').append(schbtn);
-    $('#column-content').append(wbgmbm);
-    // toolbar initialization
-    $('head').append(andmbm);
-    if ($('#content').css('color').isBright) { $('head').append(iphmbm); }
-    $('#column-content').addClass(viscls);
-    $('#footer').addClass(viscls);
-    // wiki navigation menu
-    $('#p-navicon').click(function(navmbm) {
-        navmbm.preventDefault();
-        $('#column-one').toggleClass(viscls);
-        $('#column-content').toggleClass(viscls);
-        $('#footer').toggleClass(viscls);
-        $('#p-search').removeClass(viscls);
-        $('#column-one').removeClass(schcls);
-        $('.portlet').removeClass(viscls);
-    });
-    // search functionality
-    $('#p-schicon').click(function(schmbm) {
-        schmbm.preventDefault();
-        $('#p-search').toggleClass(viscls);
-        $('#column-one').toggleClass(schcls);
-    });
-    $('#searchInput').attr('placeholder','Search the ' + wgSitename);
-    // wiki submenu navigation
-    $('.portlet > h5').click(function(menumbm) {
-        menumbm.preventDefault();
-        $(this).parent().toggleClass(viscls);
-    });
-    // content adaptability
-    $('p > br:only-child').parent().remove();
-    $(window).resize(function() {
-        if ($('.wikia-slideshow-images').length) {
-            $('.wikia-slideshow-images').each(function(mbmslideshow) {
-                $(this).css('width', $(this).parent().width());
-                $(this).css('height', $(this).parent().width()*0.75);
-            });
-        }
-    });
+});
