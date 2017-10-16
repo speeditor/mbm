76e1ef2ca62cd4ecb3ff26d50bd9af23057e7b8f
Speedit <speeditwikia@gmail.com>
2017-10-16 21:19:47 +0100

    dynamic styling for community header and subtitle

--- app.js
+++ app.js
@@ -5,40 +5,54 @@ mw.loader.using(['mediawiki.util', 'jquery']).then(function () {
 
     // == interface ==
     mw.hook('dev.colors').add(function(module) {
-        dev.colors.css(
-        '.skin-monobook.mbm-on {' +
-            'background-color: $body;' +
-            'background-image: url("/wiki/Special:FilePath/Wiki-background") !important;' +
-        '}' +
-        '.skin-monobook.mbm-on #column-content #mbm-bg,' +
-        '.skin-monobook.mbm-on #column-content textarea,' +
-        '.skin-monobook.mbm-on #column-content input,' +
-        '.skin-monobook.mbm-on #column-content select,' +
-        '.skin-monobook.mbm-on.action-edit #column-content #wpTextbox1 {' +
-            'background-color: $page;' +
-        '}' +
-        '.skin-monobook.mbm-on #column-content #mbm-bg {' +
-            'opacity: ' + wgSassParams['page-opacity']/100 + ';' +
-        '}' +
-        '.skin-monobook.mbm-on #content #firstHeading,' +
-        '.skin-monobook.mbm-on #content #siteSub,' +
-        '.skin-monobook.mbm-on #column-one .portlet,' +
-        '.skin-monobook.mbm-on #footer {' +
-            'background: $header;' +
-        '}' +
-        '.skin-monobook.mbm-on #column-one #p-logo {' +
-            'border-top: 4px solid $header !important;' +
-        '}' +
-        '.skin-monobook.mbm-on #column-one #p-logo a,' +
-        '.skin-monobook.mbm-on #column-one #p-logo a:hover {' +
-            'background-image: url("/wiki/Special:FilePath/Wiki-wordmark.png") !important;' +
-        '}' +
-        '.skin-monobook.mbm-on #globalWrapper a {' +
-            'color: $link;' +
-        '}' +
-        '.skin-monobook.mbm-on.mw-special-Upload #mw-upload-form fieldset {' +
-            'border: 1px solid $link;' +
-        '}')
+        var customColors = {
+            headerText: dev.colors.parse(dev.colors.wikia.header).isBright() ?
+                '#000000' :
+                '#ffffff',
+            headerAlpha: dev.colors.parse(dev.colors.wikia.header).rgb().replace('(','a(').replace(')', ', 0.75)'),
+            pageOpacity: wgSassParams['page-opacity']/100
+        };
+        var mbmStyles = $(mw.util.addCSS(
+            '.skin-monobook.mbm-on {' +
+                'background-color: ' + dev.colors.wikia.body + ';' +
+                'background-image: url("/wiki/Special:FilePath/Wiki-background") !important;' +
+            '}' +
+            '.skin-monobook.mbm-on #column-content #mbm-bg,' +
+            '.skin-monobook.mbm-on #column-content textarea,' +
+            '.skin-monobook.mbm-on #column-content input,' +
+            '.skin-monobook.mbm-on #column-content select,' +
+            '.skin-monobook.mbm-on.action-edit #column-content #wpTextbox1 {' +
+                'background-color: ' + dev.colors.wikia.page + ';' +
+            '}' +
+            '.skin-monobook.mbm-on #column-content #mbm-bg {' +
+                'opacity: ' + customColors.pageOpacity + ';' +
+            '}' +
+            '.skin-monobook.mbm-on #column-one #p-logo,' +
+            '.skin-monobook.mbm-on #column-content #firstHeading,' +
+            '.skin-monobook.mbm-on #column-content #siteSub,' +
+            '.skin-monobook.mbm-on #column-content #contentSub,' +
+            '.skin-monobook.mbm-on #column-content #contentSub2,' +
+            '.skin-monobook.mbm-on #column-one .portlet,' +
+            '.skin-monobook.mbm-on #footer {' +
+                'background-color: ' + dev.colors.wikia.header + ';' +
+            '}' +
+            '.skin-monobook.mbm-on #column-one #p-logo {' +
+                'background-image: url("/wiki/Special:FilePath/File:Community-header-background") !important;' +
+            '}' +
+            '.skin-monobook.mbm-on #column-one #p-logo:after {' +
+                'background: linear-gradient(to right, ' + customColors.headerAlpha + ', ' + customColors.headerAlpha + '), linear-gradient(to left, transparent 69px, ' + dev.colors.wikia.header + ' 163px);' +
+            '}' +
+            '.skin-monobook.mbm-on #column-one #p-logo a,' +
+            '.skin-monobook.mbm-on #column-one #p-logo a:hover {' +
+                'background-image: url("/wiki/Special:FilePath/Wiki-wordmark.png") !important;' +
+            '}' +
+            '.skin-monobook.mbm-on #globalWrapper a {' +
+                'color: ' + dev.colors.wikia.link + ';' +
+            '}' +
+            '.skin-monobook.mbm-on.mw-special-Upload #mw-upload-form fieldset {' +
+                'border: 1px solid ' + dev.colors.wikia.link + ';' +
+            '}')
+        ).attr('id', 'mbm-styles');
         if (navigator.userAgent.indexOf('Android') > -1) {
             $('head').append(mw.html.element('meta', { name: 'theme-color', content: dev.colors.wikia.header }));
         } else if (navigator.userAgent.indexOf('iPhone') > -1) {
